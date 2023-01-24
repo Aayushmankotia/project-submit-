3fef39fb7b75804de79a8dc08f882ea1455f6939
AAYUSH-MANKOTIA <aayushmankotiya@gmail.com>
2023-01-24 11:23:46 +0530

    add new file

---
+++ practiceregister.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practice</title>
+
+</head>
+<body>
+    <form action="practice.php" method="POST">
+        <fieldset>
+         REGISTRATION   
+        </fieldset>
+        <label for=""> NAME
+            <input type="text" name="name">
+        </label>
+
+        <label for="">E-MAIL
+            <input type="email" name="email">
+        </label>
+
+        <label for="">PASSWORD
+            <input type="password" name="pass">   
+
+        </label>
+                <input type="submit" name="submit" value="submit">
+    
+    </form>
+
+
+    
+    
+</body>
+</html>
